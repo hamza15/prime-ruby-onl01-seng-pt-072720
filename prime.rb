4c5fa9cf9dc24f6d0a4ac7760ce8d49c9424b4a8
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-02 03:41:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(value)
   if value <= 1 
     return false
   else
-    2..value-1.each do |value|
-      if value 
+    2..value-1.each do |n|
+      if n % 
       return false
   else
     return false
