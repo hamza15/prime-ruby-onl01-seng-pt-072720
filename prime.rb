3f38cf4e57d4faa3e5c17a7638f02d4d153d8163
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-02 03:41:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 # Add  code here!
 def prime?(value)
-  if value % 2 == 0 
-    return true
+  if value <= 1 
+    return false
+  else
+    2..value-1.each do |value|
+      if value 
+      return false
   else
     return false
   end
