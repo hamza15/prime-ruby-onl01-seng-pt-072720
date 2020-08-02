49a6b7ced5ff7c118957601e26c506f4cfecc41e
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-02 03:44:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(value)
+  if value <= 1 
+    return false
+  else
+    (2..(value-1)).each do |n|
+      return false if value % n == 0
+    end
+  end
+  return true
+end
